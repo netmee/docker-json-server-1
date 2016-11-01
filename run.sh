ca31e56ee38a25fa0b9f9228c4fd3101e869aaa0
Nicolas Krzyzanowski <nk@netmee.com>
2016-11-02 00:20:44 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,10 +12,4 @@ if [ -f $file ]; then
     args="$args db.json"
 fi
 
-file=/data/file.js
-if [ -f $file ]; then
-    echo "Found file.js seed file, trying to open"
-    args="$args file.js"
-fi
-
 json-server $args
